b68f73cb575f2a07a9bd80a9eeeccd9ed53efe81
Jethro Parker <lionbearjet@hotmail.com>
2018-07-06 02:10:20 +0800

    Updated to use the Firebase DB

--- YellowFox/MainWindow.xaml.cs
+++ YellowFox/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace YellowFox
 {
@@ -24,5 +16,34 @@ namespace YellowFox
         {
             InitializeComponent();
         }
+
+        private async void Window_LoadedAsync(object sender, RoutedEventArgs e)
+        {
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyDvTZmLrg8PgrXzo0Gpu7MMMr4EI2JCLlg"));
+            FirebaseAuthLink auth = await authProvider.SignInWithEmailAndPasswordAsync("generic.man@banterbun.com", "generic.man");
+
+            var firebase = new FirebaseClient(
+              "https://stuff-7ea31.firebaseio.com/",
+              new FirebaseOptions
+              {
+                  AuthTokenAsyncFactory = () => Task.FromResult(auth.FirebaseToken)
+              });
+
+            await firebase.Child("members").Child("t-rex").PutAsync(new Member());
+
+
+            var members = await firebase.Child("members").OnceAsync<Member>();
+
+            foreach (var member in members)
+            {
+                Debug.WriteLine($"{member.Object.firstname} {member.Object.lastname}");
+            }
+        }
+    }
+
+    class Member
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
     }
 }
